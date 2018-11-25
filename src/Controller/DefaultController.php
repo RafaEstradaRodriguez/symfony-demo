96cf51d54b9d2288c2dbfd541bf97382909c114b
kiko <kiko@el-mundo.net>
2018-11-25 17:18:46 +0100

    Add asset component

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -3,15 +3,26 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     /**
-     * @Route("/")
+     * @Route("/edit/{id}", name="editar_usuario")
+     */
+    public function edit($id)
+    {
+        return $this->render('other.html.twig', ['nombre' => 'John']);
+    }
+
+    /**
+     * @Route("/", name="homepage")
      */
     public function index()
     {
-        return $this->render('other.html.twig', ['nombre' => 'John']);
+        return new JsonResponse();
+        return $this->json(['result' => 'ok']);
     }
 }
