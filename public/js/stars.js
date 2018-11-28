676094194c762e27ede119553cab6e9e941b135e
Moises Carretero <moycarretero@gmail.com>
2018-11-28 15:56:26 +0100

    Save likes

--- public/js/stars.js
+++ public/js/stars.js
@@ -1,11 +1,16 @@
 document.querySelectorAll(".fa-star").forEach( estrella => {
     estrella.addEventListener('click', (event) => {
         event.preventDefault();
-        const url = estrella.getAttribute('data-url');
+        var url = estrella.getAttribute('data-url');
         fetch(url).then(response => {
             response.json().then(content => {
                 estrella.classList.toggle('fas');
                 estrella.parentNode.append(content.stars);
+                if (url.indexOf('unstar')>-1){
+                    estrella.setAttribute('data-url', url.replace('unstar', 'star'));
+                } else {
+                    estrella.setAttribute('data-url', url.replace('star', 'unstar'));
+                }
                console.log(content.stars);
             });
         })
