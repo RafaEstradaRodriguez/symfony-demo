196ef4979b014dc8457388e51b6aef50134ec650
kiko <kiko@el-mundo.net>
2018-12-17 23:43:44 +0100

    Add form register

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use App\Security\LoginFormAuthenticator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
@@ -22,6 +30,26 @@ class SecurityController extends AbstractController
             'last_username' => $lastUsername,
             'error' => $error
         ]);
+    }
+
+    /**
+     * @Route("/register", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guard, LoginFormAuthenticator $formAuthenticator, EntityManagerInterface $em)
+    {
+        //TODO hacer más comprobaciones
+
+        if ($request->isMethod('POST')) {
+            $usuario = new User();
+            $usuario->setEmail($request->request->get('email'));
+            $usuario->setPassword($passwordEncoder->encodePassword($usuario, $request->request->get('password')));
+            $em->persist($usuario);
+            $em->flush();
+
+            return $guard->authenticateUserAndHandleSuccess($usuario, $request, $formAuthenticator, 'main');
+        }
+
+        return $this->render('security/register.html.twig', []);
     }
 
     /**
